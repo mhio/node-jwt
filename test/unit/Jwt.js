40d6351b326e7255d3a44b8f48c615c1eeb64030
mhio <37205532+mhio@users.noreply.github.com>
2019-11-27 14:01:16 +1100

    Test coverage

--- test/unit/Jwt.js
+++ test/unit/Jwt.js
@@ -1,4 +1,5 @@
 /* global expect */
+const path = require('path')
 const { Jwt } = require('../../lib/Jwt')
 const {
   RE_PUB_KEY,
@@ -41,12 +42,67 @@ describe('unit::jwt::Jwt', function(){
       expect(res).to.match(RE_JWT_TOKEN)
     })
 
+    it('should fail to sign a token without data', async function(){
+      return expect(jwt.sign(undefined)).to.be.rejectedWith('Authentication Error')
+    })
+
     it('should verify a signed token', async function () {
       const token = await jwt.sign({ bool: true })
       const res = await jwt.verify(token)
       expect(res).to.containSubset({ bool: true })
     })
 
+    it('should fail to use a bad algo', function(){
+      const fn = () => new Jwt({ jwt_algorithm: 'sdafkasl' })
+      expect(fn).to.throw('Algorithm not supported')
+    })
+
+    it('should fail to use an unsupported algo', function(){
+      const fn = () => new Jwt({ jwt_algorithm: 'ES256' })
+      expect(fn).to.throw('Algorithm not supported')
+    })
+
+    it('should fail to use none algo', function(){
+      const fn = () => new Jwt({ jwt_algorithm: 'none' })
+      expect(fn).to.throw('Algorithm not supported')
+    })
+
+    it('should generate an rsa key', async function(){
+      jwt = new Jwt({ jwt_algorithm: 'RS256' })
+      await jwt.rotate_promise
+      expect(jwt.jwt_sign_secret).to.match(RE_PRIVATE_KEY)
+      expect(jwt.jwt_verify_secret).to.match(RE_PUB_KEY)
+    })
+
+    it('should fail to use RSA with only private key', async function(){
+      const fn = () => new Jwt({ jwt_algorithm: 'RS256', jwt_private_key: 'test' })
+      expect(fn).to.throw('Supply a public key ')
+    })
+
+    it('should fail to use RSA with only public key', async function(){
+      const fn = () => new Jwt({ jwt_algorithm: 'RS256', jwt_public_key: 'test' })
+      expect(fn).to.throw('Supply a private key ')
+    })
+
+    it('should use a public/private key file', async function(){
+      jwt = new Jwt({
+        jwt_algorithm: 'RS256',
+        jwt_private_key_path: path.join(__dirname,'..','fixture','jwtRS256.key'),
+        jwt_public_key_path: path.join(__dirname, '..', 'fixture','jwtRS256.key.pub'),
+      })
+      expect(jwt.jwt_sign_secret).to.match(RE_PRIVATE_KEY)
+      expect(jwt.jwt_verify_secret).to.match(RE_PUB_KEY)
+    })
+
+    it('should set a default sign option', function(){
+      jwt.setJwtSignDefaultOption('expiresIn', '2 days')
+      expect(jwt.default_jwt_sign_options).to.containSubset({ expiresIn: '2 days' })
+    })
+
+    it('should set a default verify option', function(){
+      jwt.setJwtVerifyDefaultOption('maxAge', '2 days')
+      expect(jwt.default_jwt_verify_options).to.containSubset({maxAge: '2 days'})
+    })
   })
 
 })
