4de14ae83d41fe15547257c49a4f729d74e9dfbf
mhio <37205532+mhio@users.noreply.github.com>
2019-11-27 14:01:16 +1100

    Test coverage

--- test/fixture/regex.js
+++ test/fixture/regex.js
@@ -1,5 +1,5 @@
-const RE_PUB_KEY = /-----BEGIN PUBLIC KEY-----[A-Z0-9/+=\n]{200,}-----END PUBLIC KEY-----/im
-const RE_PRIVATE_KEY = /-----BEGIN PRIVATE KEY-----[A-Z0-9/+=\n]{350,}-----END PRIVATE KEY-----/im
+const RE_PUB_KEY = /-----BEGIN (?:RSA )?PUBLIC KEY-----[A-Z0-9/+=\n]{200,}-----END (?:RSA )?PUBLIC KEY-----/im
+const RE_PRIVATE_KEY = /-----BEGIN (?:RSA )?PRIVATE KEY-----[A-Z0-9/+=\n]{350,}-----END (?:RSA )?PRIVATE KEY-----/im
 const RE_JWT_TOKEN = /^[A-Za-z0-9]{36}\.[A-Za-z0-9]+\.[-_A-Za-z0-9]{43}$/
 
 module.exports = { 
